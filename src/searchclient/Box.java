25fc5855761cfc3202bf5b5dec6634f63480f1e4
Georg <georg.h.k@hotmail.com>
2017-05-13 19:58:20 +0200

    no message

--- src/searchclient/Box.java
+++ src/searchclient/Box.java
@@ -1,41 +1,48 @@
 package searchclient;
 
 public class Box implements Comparable<Box> {
-	
+
 	public Character name;
 	public String color;
 	public Position position;
-    public boolean isBlocking = false;
+	public boolean isBlocking = false;
 
-	
 	public Box(Character name, String color, Position position) {
 		super();
 		this.name = name;
 		this.color = color;
 		this.position = position;
 	}
-	
-	public Box(Character name, String color, Position position,boolean isBlocking) {
+
+	public Box(Character name, String color, Position position, boolean isBlocking) {
 		super();
 		this.name = name;
 		this.color = color;
 		this.position = position;
 		this.isBlocking = isBlocking;
 	}
-	
-	public Box(Box b){
+
+	public Box(Box b) {
 		super();
 		this.name = b.name;
 		this.color = new String(b.color);
-		this.position = new Position(b.position.row,b.position.col);
+		this.position = new Position(b.position.row, b.position.col);
 		this.isBlocking = isBlocking;
+	}
+
+	public boolean isOnOwnGoal() {
+		for (Goal g : SearchClient.allGoals) {
+			if (this.position.equals(g.position) && g.name.equals(Character.toLowerCase(this.name))) {
+				return true;
+			}
+		}
+		return false;
 	}
 
 	@Override
 	public String toString() {
-		return "Box [name=" + name + ", color=" + color + ", position=" + position +", isBlocking="+isBlocking+ "]";
+		return "Box [name=" + name + ", color=" + color + ", position=" + position + ", isBlocking=" + isBlocking + "]";
 	}
-	
 
 	@Override
 	public int hashCode() {
